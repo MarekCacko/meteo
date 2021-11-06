0d5cd2c4ebcdc487a951753ace4b497c815e77f8
Marek Čačko <marek@marekcacko.sk>
2021-11-06 23:47:28 +0100

    Remove not used code

--- src/store/weatherSlice.ts
+++ src/store/weatherSlice.ts
@@ -53,37 +53,3 @@ export const getWeather = (state: RootState) => {
 }
 
 export default weatherSlice.reducer
-
-/*
-
-// First, define the reducer and action creators via `createSlice`
-const usersSlice = createSlice({
-  name: 'users',
-
-  reducers: {
-    usersLoading(state, action) {
-      // Use a "state machine" approach for loading state instead of booleans
-      if (state.loading === 'idle') {
-        state.loading = 'pending'
-      }
-    },
-    usersReceived(state, action) {
-      if (state.loading === 'pending') {
-        state.loading = 'idle'
-        state.users = action.payload
-      }
-    },
-  },
-})
-
-// Destructure and export the plain action creators
-export const { usersLoading, usersReceived } = usersSlice.actions
-
-// Define a thunk that dispatches those action creators
-const fetchUsers = () => async (dispatch) => {
-  dispatch(usersLoading())
-  const response = await usersAPI.fetchAll()
-  dispatch(usersReceived(response.data))
-}
-
-*/
