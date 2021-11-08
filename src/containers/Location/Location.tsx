97a4c7c2633805e4efdac2c6afbf3e0e45f71e79
Marek Čačko <marek@marekcacko.sk>
2021-11-08 15:11:18 +0100

    Add toggle to hide location container (#9)

--- src/containers/Location/Location.tsx
+++ src/containers/Location/Location.tsx
@@ -8,6 +8,7 @@ import styles from './Location.module.css'
 
 interface Props {
   changeCity: (city: ApiData) => void
+  visible: boolean
 }
 
 const Location = (props: Props) => {
@@ -17,40 +18,44 @@ const Location = (props: Props) => {
 
   const debouncedFilter = useDebounce(filter, 200)
 
-  const { changeCity } = props
+  const { changeCity, visible } = props
 
   return (
-    <section className={styles.locationContainer}>
-      <header className={styles.header}>Location</header>
-      <div className={styles.filter}>
-        <input
-          type="text"
-          placeholder="Search city ..."
-          defaultValue={filter}
-          onChange={(e) => setFilter(e.target.value)}
-        />
-      </div>
-      <div className={styles.cities} role="menu">
-        {weather
-          .filter((city) => city.name.toLowerCase().includes(debouncedFilter.toLowerCase()))
-          .sort((a, b) => a.name.localeCompare(b.name))
-          .map((city) => {
-            return (
-              <div
-                key={city.id}
-                className={styles.city}
-                onKeyDown={() => changeCity(city)}
-                onClick={() => changeCity(city)}
-                role="menuitem"
-                tabIndex={0}
-              >
-                <div className={styles.name}>{city.name}</div>
-                <div className={styles.temperature}>{Math.round(city.main.temp)}°C</div>
-              </div>
-            )
-          })}
-      </div>
-    </section>
+    <>
+      {visible && (
+        <section className={styles.locationContainer}>
+          <header className={styles.header}>Location</header>
+          <div className={styles.filter}>
+            <input
+              type="text"
+              placeholder="Search city ..."
+              defaultValue={filter}
+              onChange={(e) => setFilter(e.target.value)}
+            />
+          </div>
+          <div className={styles.cities} role="menu">
+            {weather
+              .filter((city) => city.name.toLowerCase().includes(debouncedFilter.toLowerCase()))
+              .sort((a, b) => a.name.localeCompare(b.name))
+              .map((city) => {
+                return (
+                  <div
+                    key={city.id}
+                    className={styles.city}
+                    onKeyDown={() => changeCity(city)}
+                    onClick={() => changeCity(city)}
+                    role="menuitem"
+                    tabIndex={0}
+                  >
+                    <div className={styles.name}>{city.name}</div>
+                    <div className={styles.temperature}>{Math.round(city.main.temp)}°C</div>
+                  </div>
+                )
+              })}
+          </div>
+        </section>
+      )}
+    </>
   )
 }
 
